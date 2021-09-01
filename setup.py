2312e5a45a2fee431d3f1abd99bf6786b57bc877
Ryan Oldenburg <ryan@guzba.com>
2021-08-31 22:38:31 -0500

    pypi started

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="pixie-python",
+    version="0.1.0",
+    author="Andre von Houck",
+    author_email="starplant@gmail.com",
+    description="Python bindings for Pixie, a full-featured 2d graphics library",
+    url="https://github.com/treeform/pixie-python",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    include_package_data=True,
+    package_data={"pixie": ["pixie.dll"]},
+    python_requires=">=3.6.0",
+)
