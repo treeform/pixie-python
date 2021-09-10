3dbb05b998b1e9a73c3c893503e1f5e3b7952225
treeform <starplant@gmail.com>
2021-09-09 18:01:57 -0700

    Fix package dir and description.

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name = "pixie-python",
-    version = "0.1.0",
+    version = "0.1.3",
     author = "Andre von Houck",
     author_email = "starplant@gmail.com",
     description = "Python bindings for Pixie, a full-featured 2D graphics library",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
     url = "https://github.com/treeform/pixie-python",
-    package_dir = {"": "src"},
+    package_dir = {"pixie": "src/pixie"},
     packages = setuptools.find_packages(where="src"),
     include_package_data = True,
     package_data = {"pixie": ["pixie.dll", "libpixie.so", "libpixie.dylib"]},
